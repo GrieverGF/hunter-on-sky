b0f295415bf04265790ad7d1b4688acfd8a32f2f
Andres Pulido <apulido@tul.com.co>
2021-08-25 10:29:58 -0500

    link misp and thehive

--- scripts/enable_cortex_analizers.sh
+++ scripts/enable_cortex_analizers.sh
@@ -2,30 +2,12 @@
 CORTEX_APP_URL='127.0.0.1'
 API_KEY='vRwsz7ni9nwMIUIb9c3n0WUvFF/kvEnw'
 
-#create org
-curl -XPOST -H "Authorization: Bearer $API_KEY" -H 'Content-Type: application/json' "http://$CORTEX_APP_URL:9001/api/organization" -d '{
-  "name": "DemoOrg",
-  "description": "Demo organization",
-  "status": "Active"
-}'
-
-# Create Admnin ORG
-curl -XPOST -H "Authorization: Bearer $API_KEY" -H 'Content-Type: application/json' "http://$CORTEX_APP_URL:9001/api/user" -d '{
-  "name": "Demo org Admin",
-  "roles": [
-    "read",
-    "analyze",
-    "orgadmin"
-  ],
-  "organization": "DemoOrg",
-  "login": "hive2cortex"
-}'
 
 #  set adming org key
-hive_key=$(curl -XPOST -H "Authorization: Bearer $API_KEY" "http://$CORTEX_APP_URL:9001/api/user/hive2cortex/key/renew") 
-sed -i "s#REPLACEMECORTEX#${hive_key}#g" thehive/application.conf
+hive_key=$(curl -XPOST -H "Authorization: Bearer $API_KEY" "http://$CORTEX_APP_URL:9001/api/user/hive2cortex/key") 
+echo $hive_key
 # Restart service
-sudo docker restart thehive4
+#sudo docker restart thehive4
 
 
 
