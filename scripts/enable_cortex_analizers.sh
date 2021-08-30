8bdc24d67fb7c19cd99650da8fdbb7c95932772d
Andres Pulido <apulido@tul.com.co>
2021-08-29 21:44:12 -0500

    add volumes

--- scripts/enable_cortex_analizers.sh
+++ scripts/enable_cortex_analizers.sh
@@ -4,10 +4,25 @@ API_KEY='vRwsz7ni9nwMIUIb9c3n0WUvFF/kvEnw'
 
 
 #  set adming org key
-hive_key=$(curl -XPOST -H "Authorization: Bearer $API_KEY" "http://$CORTEX_APP_URL:9001/api/user/hive2cortex/key") 
-echo $hive_key
+adminorg_key=$(curl -H "Authorization: Bearer $API_KEY" "http://$CORTEX_APP_URL:9001/api/user/hive2cortex/key") 
+echo $adminorg_key
+
+
+#curl -H "Authorization: Bearer $adminorg_key" "http://$CORTEX_APP_URL:9001/api/analyzer?range=all"
+
 # Restart service
 #sudo docker restart thehive4
 
 
+curl -XPOST -H "Authorization: Bearer $adminorg_key" "http://$CORTEX_APP_URL:9001/api/organization/analyzer/Abuse_Finder" -d '{
+  "name": "Abuse_Finder",
+  "configuration": {
+    "auto_extract_artifacts": false,
+    "check_tlp": true,
+    "max_tlp": 2
+  },
+  "rate": 1000,
+  "rateUnit": "Day",
+  "jobCache": 5
+}'
 
