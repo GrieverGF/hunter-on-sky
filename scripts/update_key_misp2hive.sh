12c268fe93076d3749b4dcdc9dcf8014beef8f56
Andres Pulido <apulido@tul.com.co>
2021-09-01 17:26:34 -0500

    fix path scripts

--- scripts/update_key_misp2hive.sh
+++ scripts/update_key_misp2hive.sh
@@ -6,6 +6,6 @@ misp_key=$(sudo docker exec -u 0 -it db  mysql --user="misp" --password="example
 # Set MISP key
 misp_key=$(sed '1,2d' <<< $misp_key)
 misp_key=$(echo ${misp_key//[$'\t\r\n ']})
-sed -i "s/REPLACEMEMISP/${misp_key}/g" /opt/hunter-on-the-sky/opt/hunter-on-the-sky/thehive/application.conf
+sed -i "s/REPLACEMEMISP/${misp_key}/g" /opt/hunter-on-the-sky/thehive/application.conf
 # Restart Service
 sudo docker restart thehive4
